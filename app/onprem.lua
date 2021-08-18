b705561e547367ea9b1e3b21679f48bfe83918f0
Edvinas <edvinas108@gmail.com>
2021-08-18 11:38:32 +0300

    Expose all config options through env vars for on premise setup, add .editorconfig

--- app/onprem.lua
+++ app/onprem.lua
@@ -1,64 +1,113 @@
+-- Utility functions for fetching environment variables
+local function is_empty(string_value)
+    return string_value == nil or string_value == ""
+end
 
+local function trim(string_value)
+    return string.gsub(string_value, "%s+", "")
+end
+
+local function get_env(env_name)
+    local value = os.getenv(env_name)
+    if is_empty(value) then
+        return nil
+    end
+
+    return trim(tostring(value))
+end
+
+local function get_env_string(env_name, default_value)
+    local env_value = get_env(env_name)
+    if env_value == nil then
+        return default_value
+    end
+
+    return env_value
+end
+
+local function get_env_boolean(env_name, default_value)
+    local env_value = get_env(env_name)
+    if env_value == nil then
+        return default_value
+    end
+
+    return string.lower(env_value) == "true"
+end
+
+local function get_env_number(env_name, default_value)
+    local env_value = get_env(env_name)
+    if env_value == nil then
+        return default_value
+    end
+
+    local env_value_number = tonumber(env_value)
+    if env_value_number == nil then
+        return default_value
+    end
+
+    return env_value_number
+end
+
+-- App initialization
 box.cfg {
-	background = true,
-	listen = 3301,
-	pid_file = "/dewt/tarantool.pid",
-	memtx_dir = "/dewt/data",
-	wal_dir = "/dewt/data",
-	vinyl_dir = "/dewt/data",
-	work_dir = "/dewt/app",
-	log = "/dewt/logs/log.txt",
-	memtx_memory = 512 * 1024 *1024,
-	checkpoint_interval = 7200,
-	custom_proc_title = "drakonhub_onprem"
+    background = get_env_boolean("APP_BACKGROUND", true),
+    listen = get_env_number("APP_LISTEN", 3301),
+    pid_file = get_env_string("APP_PID_FILE", "/dewt/tarantool.pid"),
+    memtx_dir = get_env_string("APP_MEMTX_DIR", "/dewt/data"),
+    wal_dir = get_env_string("APP_WAL_DIR", "/dewt/data"),
+    vinyl_dir = get_env_string("APP_VINYL_DIR", "/dewt/data"),
+    work_dir = get_env_string("APP_WORK_DIR", "/dewt/app"),
+    log = get_env_string("APP_LOG", "/dewt/logs/log.txt"),
+    memtx_memory = get_env_number("APP_MEMTX_MEMORY", 512 * 1024 * 1024),
+    checkpoint_interval = get_env_number("APP_CHECKPOINT_INTERVAL", 7200),
+    custom_proc_title = get_env_string("APP_CUSTOM_PROC_TITLE", "drakonhub_onprem")
 }
 
 global_cfg = {
-	db = "tardb",
-	--db = "mysqldb",
-	mysql = {
-		host = "127.0.0.1",
-		db = "drakonhub",
-		user = "tara",
-		password = "123456",	
-		size = 5
-	},
-	diatest = "/dewt/diatest",
-	host = os.getenv("HOST"),
-	port = 8090,
-	http_options = {
-		log_requests = false
-	},	
-	session_timeout = 10 * 24 * 3600,
-	static_timeout = 1 * 3600,
-	file_timeout = 2,
-	static_dir = "/dewt/static",
-	emails_dir = "/dewt/emails",
-	feedback_dir = "/dewt/feedback",
-	journal_dir = "/dewt/journal",
-	content_dir = "/dewt/content",
-	read_dir = "/dewt/read",	
-	password_timeout = 5,
-	use_capture = false,
-	max_recent = 20,
-	max_log = 50000,
-	tmp = "/dewt/tmp",
-	debug_mail = false,
-	feedback_email = "bad@example.com",
-	create_license = "extended",
-	licensing = true,
-	https_sender_port = 3400,
-	google_anal = false,
-	my_site = os.getenv("MY_SITE"),
-	my_domain = os.getenv("MY_DOMAIN"),
-	my_ip = my_ip,
-	complete_delay = 2,
-	on_premises = true,
-	application = "DrakonHub",
-	insecure_cookie = os.getenv("INSECURE_COOKIE") == "true"
+    db = get_env_string("APP_DB", "tardb"),
+
+    --db = "mysqldb",
+    mysql = {
+        host = get_env_string("APP_MYSQL_HOST", "127.0.0.1"),
+        db = get_env_string("APP_MYSQL_DB", "drakonhub"),
+        user = get_env_string("APP_MYSQL_USER", "tara"),
+        password = get_env_string("APP_MYSQL_PASSWORD", "123456"),
+        size = get_env_number("APP_MYSQL_SIZE", 5)
+    },
+    diatest = get_env_string("APP_DIATEST", "/dewt/diatest"),
+    host = get_env_string("APP_HOST", "127.0.0.1"),
+    port = get_env_number("APP_PORT", 8090),
+    http_options = {
+        log_requests = get_env_boolean("APP_HTTP_OPTIONS_LOG_REQUESTS", false)
+    },
+    session_timeout = get_env_number("APP_SESSION_TIMEOUT", 10 * 24 * 3600),
+    static_timeout = get_env_number("APP_STATIC_TIMEOUT", 1 * 3600),
+    file_timeout = get_env_number("APP_FILE_TIMEOUT", 2),
+    static_dir = get_env_string("APP_STATIC_DIR", "/dewt/static"),
+    emails_dir = get_env_string("APP_EMAILS_DIR", "/dewt/emails"),
+    feedback_dir = get_env_string("APP_FEEDBACK_DIR", "/dewt/feedback"),
+    journal_dir = get_env_string("APP_JOURNAL_DIR", "/dewt/journal"),
+    content_dir = get_env_string("APP_CONTENT_DIR", "/dewt/content"),
+    read_dir = get_env_string("APP_READ_DIR", "/dewt/read"),
+    password_timeout = get_env_number("APP_PASSWORD_TIMEOUT", 5),
+    use_capture = get_env_boolean("APP_USE_CAPTURE", false),
+    max_recent = get_env_number("APP_MAX_RECENT", 20),
+    max_log = get_env_number("APP_MAX_LOG", 50000),
+    tmp = get_env_string("APP_TMP", "/dewt/tmp"),
+    debug_mail = get_env_boolean("APP_DEBUG_MAIL", false),
+    feedback_email = get_env_string("APP_FEEDBACK_EMAIL", "bad@example.com"),
+    create_license = get_env_string("APP_CREATE_LICENSE", "extended"),
+    licensing = get_env_boolean("APP_LICENSING", true),
+    https_sender_port = get_env_boolean("APP_HTTPS_SENDER_PORT", 3400),
+    google_anal = get_env_boolean("APP_GOOGLE_ANAL", false),
+    my_site = get_env_string("APP_MY_SITE", "127.0.0.1"),
+    my_domain = get_env_string("APP_MY_DOMAIN", "https://127.0.0.1"),
+    my_ip = my_ip,
+    complete_delay = get_env_number("APP_COMPLETE_DELAY", 2),
+    on_premises = get_env_boolean("APP_ON_PREMISES", true),
+    application = get_env_string("APPLICATION", "DrakonHub"),
+    insecure_cookie = get_env_boolean("APP_INSECURE_COOKIE", false)
 }
 
 external_creds = require("external_creds")
 require("init")
-
-
